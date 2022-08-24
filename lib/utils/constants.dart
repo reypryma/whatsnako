e5f57d9a42b888b0073e36c68d39718240f52be3
repryma <refanopradana@gmail.com>
2022-08-25 03:14:49 +0700

    All

---
+++ lib/utils/constants.dart
@@ -0,0 +1,5 @@
+//Image Location
+const String BOCCHI_IMAGES_URL = 'assets/bocchi';
+const String DEFAULT_IMAGES_URL = 'assets/images';
+
+const String APPS_NAME = 'Whats Nako';
---
+++ lib/utils/images.dart
@@ -0,0 +1,13 @@
+import 'constants.dart';
+
+class WhatsNakoImage {
+
+  //Main Image
+  static const String bgImageWAFull = '$DEFAULT_IMAGES_URL/bg1.png';
+  static const String bgImageFavFull = '$DEFAULT_IMAGES_URL/Image_24.webp';
+
+  //Profile
+  static const String profileBocchi = '$BOCCHI_IMAGES_URL/haa.png';
+  static const String profileSotockha = '$BOCCHI_IMAGES_URL/sotoka.png';
+
+}
